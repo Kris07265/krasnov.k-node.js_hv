59b18ed9d84736e9e820b3c3333d99dbc221c8cd
Kris <krasnovkristian07@gmail.com>
2026-07-04 14:54:12 +0300

    hw projects

---
+++ projects/nodejs/websocket-hw/ws-server.js
@@ -0,0 +1,23 @@
+const { WebSocketServer } = require('ws');
+
+const wss = new WebSocketServer({ port: 8080 });
+
+console.log('WebSocket-сервер запущено: ws://localhost:8080');
+
+wss.on('connection', (socket) => {
+    console.log('Новий клієнт підключився');
+
+    socket.on('message', (data) => {
+        const text = data.toString();
+        const time = new Date().toLocaleTimeString('uk-UA');
+        const message = `[${time}] ${text}`;
+
+        wss.clients.forEach((client) => {
+            client.send(message);
+        });
+    });
+
+    socket.on('close', () => {
+        console.log('Клієнт відключився');
+    });
+});
