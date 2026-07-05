22b2859d37d6410aedfbc13c5c4642136b9f0148
Kris <krasnovkristian07@gmail.com>
2026-07-05 18:38:13 +0300

    Finished hw 11

--- study-task-tracker/modules/fileStorage.js
+++ study-task-tracker/modules/fileStorage.js
@@ -6,22 +6,46 @@ const tasksFilePath = path.join(dataDirPath, 'tasks.json');
 
 const initStorage = () => {
     if(!fs.existsSync(dataDirPath)){
-        fs.mkdirSync(dataDirPath)
+        fs.mkdir(dataDirPath, (err) => {
+            if(err){
+                console.error("Error creating folder");
+                return;
+            }
+            console.log("Creating folder successfully");
+        });
     }
 
     if (!fs.existsSync(tasksFilePath)){
-        fs.writeFileSync(tasksFilePath, '[]')
+        fs.writeFile(tasksFilePath, '[]', (err) => {
+            if(err){
+                console.error("Error creating file");
+                return;
+            }
+            console.log("Creating file successfully");
+        });
     }
 }
 
 const readTasks = () => {
-    const file = fs.readFileSync(tasksFilePath, 'utf8')
+    const file = fs.readFile(tasksFilePath, 'utf8', (err, data) => {
+        if(err){
+            console.error("Error reading file");
+            return;
+        }
+        console.log("File contents:", data);
+    });
     return JSON.parse(file);
 }
 
 const saveTasks = (tasks) => {
     const file = JSON.stringify(tasks, null, 2);
-    fs.writeFileSync(tasksFilePath, file)
+    fs.writeFile(tasksFilePath, file, (err) => {
+        if(err){
+            console.error("Error saving tasks");
+            return;
+        }
+        console.log("Saved tasks");
+    });
 }
 
 module.exports = {
